20cdc1ba013f8c00cadd6f4118f5586e4c7a9b79
Ezri-Adiel <250197@utxicotepec.edu.mx>
2026-09-15 11:09:53 -0600

    subir practicas

---
+++ arreglo.py
@@ -0,0 +1,18 @@
+# Crear un arreglo
+numeros = [10, 20, 30, 40, 50]
+
+# Mostrar el arreglo
+print(numeros)
+
+# Acceder a un elemento
+print(numeros[0])  # 10
+
+# Agregar un elemento
+numeros.append(60)
+
+# Modificar un elemento
+numeros[1] = 25
+
+# Recorrer el arreglo
+for numero in numeros:
+    print(numero)
