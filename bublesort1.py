91c647a8866c4ea2cf0c19911eb98ba6beb4729a
Ezri-Adiel <250197@utxicotepec.edu.mx>
2026-09-17 13:58:45 -0600

    Add files via upload

---
+++ bublesort1.py
@@ -0,0 +1,10 @@
+lista = [67, 5, 14, 0]
+n = len(lista)
+swapped = True
+while swapped:
+    swapped=False
+    for i in range(n-1):
+        if lista[i]>lista[i+1]:
+            lista[i],lista [i+1]=lista[i+1],lista[i]
+            swapped=True
+print("lista ordenada: ",lista)
